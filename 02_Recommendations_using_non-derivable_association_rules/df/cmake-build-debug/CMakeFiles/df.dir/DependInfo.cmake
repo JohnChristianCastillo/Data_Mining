d2b32c3a14dddf2da0848c73bc0a84f0452c89aa
JohnChristianCastillo <john.castillojandayan@student.uantwerpen.be>
2023-06-29 05:23:16 +0200

    task 2

---
+++ 02_Recommendations_using_non-derivable_association_rules/df/cmake-build-debug/CMakeFiles/df.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/yanok/Desktop/Data_Mining/02_Recommendations_using_non-derivable_association_rules/df/data.cpp" "C:/Users/yanok/Desktop/Data_Mining/02_Recommendations_using_non-derivable_association_rules/df/cmake-build-debug/CMakeFiles/df.dir/data.cpp.obj"
+  "C:/Users/yanok/Desktop/Data_Mining/02_Recommendations_using_non-derivable_association_rules/df/ndi.cpp" "C:/Users/yanok/Desktop/Data_Mining/02_Recommendations_using_non-derivable_association_rules/df/cmake-build-debug/CMakeFiles/df.dir/ndi.cpp.obj"
+  "C:/Users/yanok/Desktop/Data_Mining/02_Recommendations_using_non-derivable_association_rules/df/qie.cpp" "C:/Users/yanok/Desktop/Data_Mining/02_Recommendations_using_non-derivable_association_rules/df/cmake-build-debug/CMakeFiles/df.dir/qie.cpp.obj"
+  "C:/Users/yanok/Desktop/Data_Mining/02_Recommendations_using_non-derivable_association_rules/df/testndi.cpp" "C:/Users/yanok/Desktop/Data_Mining/02_Recommendations_using_non-derivable_association_rules/df/cmake-build-debug/CMakeFiles/df.dir/testndi.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ 02_Recommendations_using_non-derivable_association_rules/df/cmake-build-debug/CMakeFiles/df.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/df.dir/data.cpp.obj"
+  "CMakeFiles/df.dir/ndi.cpp.obj"
+  "CMakeFiles/df.dir/qie.cpp.obj"
+  "CMakeFiles/df.dir/testndi.cpp.obj"
+  "df.exe"
+  "df.exe.manifest"
+  "df.pdb"
+  "libdf.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/df.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
